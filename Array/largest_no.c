759fb26585737c102095bc8c578280288c6cef58
Ravikrp208 <ravikrp208@gmail.com>
2026-01-26 22:31:08 +0530

    find largest no

---
+++ Array/largest_no.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, max;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];   // assume first element is largest
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
