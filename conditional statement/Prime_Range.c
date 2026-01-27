a300c7000395946061bcfae9042575ba9d55c1fc
Ravikrp208 <ravikrp208@gmail.com>
2026-01-27 23:19:04 +0530

    Prime no range

---
+++ conditional statement/Prime_Range.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, count;
+
+    for (num = 1; num <= 100; num++) {
+        count = 0;
+
+        for (i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            printf("%d ", num);
+        }
+    }
+
+    return 0;
+}
