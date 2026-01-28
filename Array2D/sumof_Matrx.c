8e5b8526f9a1d23dafc2e1d1c80c0e9e3e817325
Ravikrp208 <ravikrp208@gmail.com>
2026-01-28 22:16:20 +0530

    2d matrix

--- Array2D/sumof_Matrx.c
+++ Array2D/sumof_Matrx.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int a[2] ,b [3] ,i,j;
+
+    printf("Enter a matrix ");
+    for(i=0; i<=2; i++){
+        for(j=0; j<=2; j++){
+            
+        }
+    }
 }
