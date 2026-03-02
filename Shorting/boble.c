aeea9b2adca90eb7bf377210595b3ddd3691db23
Ravikrp208 <ravikrp208@gmail.com>
2026-03-02 23:45:56 +0530

    bobble short

---
+++ Shorting/boble.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, j, temp;
+
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort logic
+    for(i = 0; i < 5 - 1; i++) {
+        for(j = 0; j < 5 - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array (Ascending order):\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
