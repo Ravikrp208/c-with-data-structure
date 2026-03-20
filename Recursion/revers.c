e5e14cb9bd6b9990404f01f445066fbc0e4661ed
Ravikrp208 <ravikrp208@gmail.com>
2026-03-20 23:14:16 +0530

    Reverse in c

---
+++ Recursion/revers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int reverse(int n, int rev) {
+    if (n == 0)
+        return rev;
+    return reverse(n / 10, rev * 10 + n % 10);
+}
+
+int main() {
+    int num = 121;
+    if (num == reverse(num, 0))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+    return 0;
+}
